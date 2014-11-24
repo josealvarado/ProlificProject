5f6a8d91312e0a71e20c5fc9954787dd3c6fab66
Jose Alvarado <josealvarado@Joses-MacBook-Pro.local>
2014-11-23 21:46:27 -0800

    Initial Commit

---
+++ ProlificProject/ProlificProject/BookListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookListTableViewController.h
+//  ProlificProject
+//
+//  Created by Jose Alvarado on 11/23/14.
+//  Copyright (c) 2014 JoseAlvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookListTableViewController : UITableViewController
+
+@end
